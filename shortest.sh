018d9cfa4a85d47b69578ebffd50b4f697dda485
ricmunz <37226713+ricmunz@users.noreply.github.com>
2021-03-26 16:25:44 -0400

    Piped grep consolidation
    Consolidated piped grep from validation into the 'valid' variable. 
    Validation from workstation requires the 'eval' command for piped grep to work properly'.

--- shortest.sh
+++ shortest.sh
@@ -2,7 +2,7 @@ url='https://raw.githubusercontent.com/ricmunz/ROLVirtualLabEnv/main/'
 apath='/etc/profile.d/bash_Color-Prompts.sh'
 cmd="wget -O $apath ${url}"
 cmd2='ssh root@server'
-valid="grep PS1 $apath"
+valid="(grep PS1 $apath) | (grep -o --color=always '1;3.m')"
 
 #Configuring workstation
 echo Configuring workstation...
@@ -18,7 +18,8 @@ ${cmd2}b ${cmd}serverb.sh
 {
 echo Validating changes...
 echo "Expected  Actual"
-printf '  1;32m  '; $valid | grep --color=always -o '1;32m'
-printf '  1;34m  '; ${cmd2}a $valid | grep --color=always -o '1;34m'
-printf '  1;33m  '; ${cmd2}b $valid | grep --color=always -o '1;33m'
+#Need to have this eval here but not sure why not for remote
+printf '  1;32m  '; eval $valid
+printf '  1;34m  '; ${cmd2}a $valid
+printf '  1;33m  '; ${cmd2}b $valid
 }
