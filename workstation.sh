1d0d4ca827c61b4caf08ca3b30b4b65dfc6ff475
ricmunz <37226713+ricmunz@users.noreply.github.com>
2021-04-09 14:58:31 -0400

    Changing display resolution too
    Implemented changing resolution to 1600x900.
    Also prints previous and updated values.

--- workstation.sh
+++ workstation.sh
@@ -2,3 +2,14 @@
 
 # Make the bash prompts bold with green color.
 export PS1='\[\e[1;32m\][\u@\h \w]\$\[\e[0m\] '
+
+# Change display resolution
+
+# Get and save current resolution in case we need to revert.
+prev_res=$(xrandr | grep \* | awk '{print $1}')
+echo "Current resolution: ${prev_res}"
+
+#Set to new resolution 1600x900 (16:9 60Hz)
+new_res="1600x900"
+xrandr --output Virtual-1 --mode ${new_res}
+echo "    New resolution: ${new_res}"
