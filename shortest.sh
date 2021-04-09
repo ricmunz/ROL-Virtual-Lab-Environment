acbf065b05b1ddae0c348815020438f38949321a
ricmunz <37226713+ricmunz@users.noreply.github.com>
2021-04-09 15:23:13 -0400

    Workstation display resolution changed here now
    Moved resolution change from 'workstation.sh' to here.
    This way, it only changes when calling this script instead of login one.
    Avoids having it echo the change every time a new interactive shell starts.

--- shortest.sh
+++ shortest.sh
@@ -4,6 +4,22 @@ cmd="wget -O $apath ${url}"
 cmd2='ssh root@server'
 valid="(grep PS1 $apath) | (grep -o --color=always '1;3.m')"
 
+# ------------------------------------------
+# Changing display resolution on workstation
+# ------------------------------------------
+
+# Get and save current resolution in case we need to revert.
+prev_res=$(xrandr | grep \* | awk '{print $1}')
+echo "Current resolution: ${prev_res}"
+# Set to new resolution 1600x900 (16:9 60Hz)
+new_res="1600x900"
+xrandr --output Virtual-1 --mode ${new_res}
+echo "    New resolution: ${new_res}"
+
+# ------------------------------
+# Now changing the shell prompts
+# ------------------------------
+
 #Configuring workstation
 echo Configuring workstation...
 ${cmd}workstation.sh
